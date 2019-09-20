30df9c6633b6ba708b89abf3bb8bdf470ab3acc8
Mia Nordentoft <mia@miestasmia.com>
2019-09-20 16:54:20 +0200

    Fix typo

--- translations.js
+++ translations.js
@@ -24,7 +24,7 @@ var translations = {
         aboutText1: "Among other things, I'm interested in technology, education, and equality in various different fields—mainly languages and LGBT+. I really just like to create stuff that matters to other people.",
         aboutText2: "I'm currently really busy with various projects, but do let me know if you have something interesting going on! I speak fluent Danish, Esperanto and English.",
         aboutText3: "I'm president of {0}the Danish Esperanto Youth (DEJO){1} and currently work at {2}the World Esperanto Youth Organization{3} in the Netherlands.",
-        aboutText4: "My resumé can be found {0}here{1}.",
+        aboutText4: "My résumé can be found {0}here{1}.",
         contactMe: "Contact me",
         contactSub: "I'd love to speak to you. Here's how",
         contact1: "You can send me a DM on Twitter. I'm {0}miestasmia{1}@miestasmia{2}.",
